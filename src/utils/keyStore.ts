65e9fe990ea5465ae13b0637ec7d6a79bca1cb9c
Sherif <ibrahimsherifdeji@gmail.com>
2025-05-08 23:13:56 +0100

    Fix keyStore parsing for 2025-04 RSA keys using newline replacement

--- src/utils/keyStore.ts
+++ src/utils/keyStore.ts
@@ -1,9 +1,7 @@
-// src/utils/keyStore.ts
-
 export const keys = {
   "key-2025-04": {
-    privateKey: process.env.PRIVATE_KEY_2025_04!,
-    publicKey: process.env.PUBLIC_KEY_2025_04!,
+    privateKey: process.env.PRIVATE_KEY_2025_04?.replace(/\\n/g, "\n")!,
+    publicKey: process.env.PUBLIC_KEY_2025_04?.replace(/\\n/g, "\n")!,
   },
 };
 
