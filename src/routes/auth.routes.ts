b0b6bf8bcd4e3dfea4ce6a296744901648699bf1
Sherif <ibrahimsherifdeji@gmail.com>
2025-05-10 22:43:44 +0100

    refactor: add proper typings

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from "express";
 import { Router } from "express";
 import { signup, signin } from "../controllers/auth.controller";
 import { verifyToken } from "../services/jwt.service";
@@ -13,20 +14,26 @@ router.get("/protected", verifyTokenMiddleware, (req, res) => {
   res.status(200).json({ message: "Access granted" });
 });
 
-export default router;
-
 // Middleware to verify the token
-function verifyTokenMiddleware(req: any, res: any, next: any) {
+function verifyTokenMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
   const token = req.headers["authorization"]?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized access" });
+    res.status(401).json({ message: "Unauthorized access" });
+    return;
   }
 
   try {
     verifyToken(token); // Verify the token using the verifyToken service
     next(); // Proceed to the protected route if token is valid
   } catch (error) {
-    return res.status(401).json({ message: "Unauthorized access" });
+    res.status(401).json({ message: "Unauthorized access" });
+    return;
   }
 }
+
+export default router;
