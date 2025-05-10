4a35258034238a9502c66a1b3c96f492745e021a
Sherif <ibrahimsherifdeji@gmail.com>
2025-05-10 22:27:41 +0100

    refactor: update keyStore to include proper key typing for kid access

--- src/utils/keyStore.ts
+++ src/utils/keyStore.ts
@@ -1,4 +1,4 @@
-export const keys = {
+export const keys: Record<string, { privateKey: string; publicKey: string }> = {
   "key-2025-04": {
     privateKey: process.env.PRIVATE_KEY_2025_04?.replace(/\\n/g, "\n")!,
     publicKey: process.env.PUBLIC_KEY_2025_04?.replace(/\\n/g, "\n")!,
